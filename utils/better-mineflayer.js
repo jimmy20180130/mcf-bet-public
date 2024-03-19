51fb9d6f811a7eb47d72ce79fb0c967cf85e041a
jimmy20180130 <68285469+jimmy20180130@users.noreply.github.com>
2024-03-20 00:20:34 +0800

    Fixed Bugs

--- utils/better-mineflayer.js
+++ utils/better-mineflayer.js
@@ -1,18 +1,75 @@
 const Vec3 = require('vec3');
 
 async function activateBlock (bot, block) {
-    const cursorPos = new Vec3(0.5, 0.5, 0.5)
-    bot._client.write('block_place', {
-        location: block.position,
-        direction: new Vec3(0, 1, 0),
-        hand: 0,
-        cursorX: cursorPos.x,
-        cursorY: cursorPos.y,
-        cursorZ: cursorPos.z,
-        insideBlock: false
-    })
+    // const cursorPos = new Vec3(0.5, 0.5, 0.5)
+    // bot._client.write('block_place', {
+    //     location: block.position,
+    //     direction: new Vec3(0, 1, 0),
+    //     hand: 0,
+    //     cursorX: cursorPos.x,
+    //     cursorY: cursorPos.y,
+    //     cursorZ: cursorPos.z,
+    //     insideBlock: false
+    // })
 
-    bot.swingArm()
+    function vectorToDirection (v) {
+        if (v.y < 0) {
+            return 0
+        } else if (v.y > 0) {
+            return 1
+        } else if (v.z < 0) {
+            return 2
+        } else if (v.z > 0) {
+            return 3
+        } else if (v.x < 0) {
+            return 4
+        } else if (v.x > 0) {
+            return 5
+        }
+    }
+
+    let direction = new Vec3(0, 1, 0)
+    const directionNum = vectorToDirection(direction) // The packet needs a number as the direction
+    let cursorPos = new Vec3(0.5, 0.5, 0.5)
+
+    if (bot.supportFeature('blockPlaceHasHeldItem')) {
+        bot._client.write('block_place', {
+            location: block.position,
+            direction: directionNum,
+            heldItem: Item.toNotch(bot.heldItem),
+            cursorX: cursorPos.scaled(16).x,
+            cursorY: cursorPos.scaled(16).y,
+            cursorZ: cursorPos.scaled(16).z
+        })
+    } else if (bot.supportFeature('blockPlaceHasHandAndIntCursor')) {
+        bot._client.write('block_place', {
+            location: block.position,
+            direction: directionNum,
+            hand: 0,
+            cursorX: cursorPos.scaled(16).x,
+            cursorY: cursorPos.scaled(16).y,
+            cursorZ: cursorPos.scaled(16).z
+        })
+    } else if (bot.supportFeature('blockPlaceHasHandAndFloatCursor')) {
+        bot._client.write('block_place', {
+            location: block.position,
+            direction: directionNum,
+            hand: 0,
+            cursorX: cursorPos.x,
+            cursorY: cursorPos.y,
+            cursorZ: cursorPos.z
+        })
+    } else if (bot.supportFeature('blockPlaceHasInsideBlock')) {
+        bot._client.write('block_place', {
+            location: block.position,
+            direction: directionNum,
+            hand: 0,
+            cursorX: cursorPos.x,
+            cursorY: cursorPos.y,
+            cursorZ: cursorPos.z,
+            insideBlock: false
+        })
+    }
 }
 
 module.exports = {
