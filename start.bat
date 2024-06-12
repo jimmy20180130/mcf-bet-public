a8ef7ab197f5b1c63b50753a933be6c1738c1398
XiaoXi_TW <68285469+jimmy20180130@users.noreply.github.com>
2024-06-12 18:30:45 +0800

    修正編碼問題

--- start.bat
+++ start.bat
@@ -1,19 +1,29 @@
 @echo off
+chcp 65001
 cls
 echo bot will restart when it crashes
 title Jimmy Bot
 
 echo checking environment
+title Jimmy Bot - checking Environment
+
+:: 檢查 Node.js 是否已安裝
+where /q node
+if %errorlevel% neq 0 (
+    echo Node.js 未安裝!
+    pause
+    exit
+)
+
 :: 安裝套件
 call npm install .
 
-title Jimmy Bot
+title Jimmy Bot - Running
 
 :StartServer
-echo (%time%) updating the bot
-start /min node update.js
-timeout /t 5
 echo (%time%) starting the bot
-start /wait node start.js
+node start.js
 echo (%time%) restarting the bot
+title Jimmy Bot - Restarting
+timeout /t 20
 goto StartServer
