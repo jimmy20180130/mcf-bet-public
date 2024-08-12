27c16d1bb6eb0d991c85f5cb1db4b6a7924fedc8
XiaoXi_TW <68285469+jimmy20180130@users.noreply.github.com>
2024-08-12 08:12:05 +0800

    Update get_player_info.js

--- utils/get_player_info.js
+++ utils/get_player_info.js
@@ -1,4 +1,5 @@
 const Logger = require('./logger');
+const axios = require('axios');
 
 // [{"uuid": "uuid", "playerid": "name", "time": 12345}]
 let uuids = [];
@@ -13,21 +14,19 @@ async function get_player_uuid(playerid) {
 
     return await new Promise(async (resolve, reject) => {
         let result;
-        await fetch(`https://api.mojang.com/users/profiles/minecraft/${playerid}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data && data.id) {
-                    result = data.id
+        await axios.get(`https://api.mojang.com/users/profiles/minecraft/${playerid}`)
+            .then(response => {
+                if (response.data && response.data.id) {
+                    result = response.data.id
                     uuids.push({"uuid": result, "playerid": playerid, "time": Date.now()})
                     Logger.debug(`[玩家資料] 玩家 ${playerid} 的 UUID: ${result}`)
-
                 } else {
-                    Logger.warn(`[玩家資料] 無法取得玩家 ${playerid} 的 UUID: ${data.errorMessage}`)
+                    Logger.warn(`[玩家資料] 無法取得玩家 ${playerid} 的 UUID: ${response.data.errorMessage}`)
                     result = 'Not Found'
                 }
             })
             .catch(error => {
-                Logger.error(`[玩家資料] 查詢玩家 UUID 時發現錯誤: ${error}`)
+                Logger.error(`[玩家賳料] 查詢玩家 UUID 時發現錯誤: ${error}`)
                 result = 'Unexpected Error'
             });
         
@@ -48,20 +47,19 @@ async function get_player_name(uuid) {
         }
     }
 
-    await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data) {
-                result = data.name
+    axios.get(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`)
+        .then(response => {
+            if (response.data) {
+                result = response.data.name
                 Logger.debug(`[玩家資料] 玩家 ${uuid} 的名稱: ${result}`)
             } else {
                 result = 'Not Found'
-                Logger.warn(`[玩家資料] 無法取得玩家 ${uuid} 的名稱: ${data.errorMessage}`)
+                Logger.warn(`[玩家賳料] 無法取得玩家 ${uuid} 的名稱: ${response.data.errorMessage}`)
             }
         })
         .catch(error => {
             result = 'Unexpected Error'
-            Logger.error(`[玩家資料] 查詢玩家名稱時發現錯誤: ${error}`)
+            Logger.error(`[玩家賳料] 查詢玩家名稱時發現錯誤: ${error}`)
         });
 
     if (result && result != '無法取得') {
