9fecf0ab9948c465376c3d84c7a130657eae60a2
XiaoXi_TW <68285469+jimmy20180130@users.noreply.github.com>
2024-08-31 12:22:20 +0800

    Update settings.js

--- discord/settings.js
+++ discord/settings.js
@@ -5,174 +5,174 @@ const { get_user_data } = require(`../utils/database.js`);
 
 module.exports = {
 	data: new SlashCommandBuilder()
-	.setName('設定')
-	.setDescription('機器人設定')
-	.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-	.setDMPermission(false)
-	.addSubcommand(subcommand =>
-		subcommand
-			.setName('對賭')
-			.setDescription('對賭設定')
-			.addIntegerOption(number =>
-				number
-					.setName('綠寶石上限')
-					.setDescription('綠寶石下注金額上限')
-					.setMinValue(1)
-			)
-			.addIntegerOption(number =>
-				number
-					.setName('綠寶石下限')
-					.setDescription('綠寶石下注金額下限')
-					.setMinValue(1)
-			)
-			.addIntegerOption(number =>
-				number
-					.setName('村民錠上限')
-					.setDescription('村民錠下注金額上限')
-					.setMinValue(1)
-			)
-			.addIntegerOption(number =>
-				number
-					.setName('村民錠下限')
-					.setDescription('村民錠下注金額下限')
-					.setMinValue(1)
-			)
-			.addNumberOption(number =>
-				number
-					.setName('綠寶石賠率')
-					.setDescription('綠寶石的賠率')
-					.setMinValue(0)
-			)
-			.addNumberOption(number =>
-				number
-					.setName('村民錠賠率')
-					.setDescription('村民錠的賠率')
-					.setMinValue(0)
-			)
-	)
+		.setName('設定')
+		.setDescription('機器人設定')
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+		.setDMPermission(false)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('對賭')
+				.setDescription('對賭設定')
+				.addIntegerOption(number =>
+					number
+						.setName('綠寶石上限')
+						.setDescription('綠寶石下注金額上限')
+						.setMinValue(1)
+				)
+				.addIntegerOption(number =>
+					number
+						.setName('綠寶石下限')
+						.setDescription('綠寶石下注金額下限')
+						.setMinValue(1)
+				)
+				.addIntegerOption(number =>
+					number
+						.setName('村民錠上限')
+						.setDescription('村民錠下注金額上限')
+						.setMinValue(1)
+				)
+				.addIntegerOption(number =>
+					number
+						.setName('村民錠下限')
+						.setDescription('村民錠下注金額下限')
+						.setMinValue(1)
+				)
+				.addNumberOption(number =>
+					number
+						.setName('綠寶石賠率')
+						.setDescription('綠寶石的賠率')
+						.setMinValue(0)
+				)
+				.addNumberOption(number =>
+					number
+						.setName('村民錠賠率')
+						.setDescription('村民錠的賠率')
+						.setMinValue(0)
+				)
+		)
 
-	.addSubcommand(subcommand =>
-		subcommand
-			.setName('機器人')
-			.setDescription('機器人設定')
-			.addIntegerOption(option =>
-				option
-					.setName('分流')
-					.setDescription('機器人所在分流')
-					.setRequired(true)
-			)
-			.addStringOption(option => 
-				option
-					.setName('家點')
-					.setDescription('機器人的家點名稱') 
-					.setRequired(true)
-			)
-	)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('機器人')
+				.setDescription('機器人設定')
+				.addIntegerOption(option =>
+					option
+						.setName('分流')
+						.setDescription('機器人所在分流')
+						.setRequired(true)
+				)
+				.addStringOption(option =>
+					option
+						.setName('家點')
+						.setDescription('機器人的家點名稱')
+						.setRequired(true)
+				)
+		)
 
-	.addSubcommandGroup(group => 
-		group
-			.setName('自動發話')
-			.setDescription('設定機器人自動發話')
-			.addSubcommand(subcommand => 
-				subcommand
-					.setName('建立')
-					.setDescription('新增自動發話的文字')
-					.addStringOption(option =>
-						option.setName('文字或指令')
-							.setDescription('自動發話的內容')
-							.setRequired(true)
-					)
-					.addIntegerOption(option =>
-						option.setName('間隔時間')
-							.setDescription('間隔時間 (以毫秒計算)')
-							.setRequired(true)
-					)
-			)
-			.addSubcommand(subcommand =>
-				subcommand
-					.setName('刪除') 
-					.setDescription('刪除自動發話的文字')
-					.addStringOption(option =>
-						option.setName('文字或指令')
-							.setAutocomplete(true)
-							.setDescription('自動發話的內容')
-					)
-			)
-	)
+		.addSubcommandGroup(group =>
+			group
+				.setName('自動發話')
+				.setDescription('設定機器人自動發話')
+				.addSubcommand(subcommand =>
+					subcommand
+						.setName('建立')
+						.setDescription('新增自動發話的文字')
+						.addStringOption(option =>
+							option.setName('文字或指令')
+								.setDescription('自動發話的內容')
+								.setRequired(true)
+						)
+						.addIntegerOption(option =>
+							option.setName('間隔時間')
+								.setDescription('間隔時間 (以毫秒計算)')
+								.setRequired(true)
+						)
+				)
+				.addSubcommand(subcommand =>
+					subcommand
+						.setName('刪除')
+						.setDescription('刪除自動發話的文字')
+						.addStringOption(option =>
+							option.setName('文字或指令')
+								.setAutocomplete(true)
+								.setDescription('自動發話的內容')
+						)
+				)
+		)
 
-	.addSubcommandGroup(group =>
-		group
-			.setName('管理員')
-			.setDescription('設定管理員')
-			.addSubcommand(subcommand =>
-				subcommand
-					.setName('新增')
-					.setDescription('新增管理員')
-					.addUserOption(option =>
-						option
-							.setName('使用者')
-							.setDescription('新增的管理員')
-							.setRequired(true)
-					)
-			)
-			.addSubcommand(subcommand =>
-				subcommand
-					.setName('移除')
-					.setDescription('刪除管理員')
-					.addUserOption(option =>
-						option
-							.setName('使用者')
-							.setDescription('刪除的管理員')
-							.setRequired(true)
-					)
-			)
-	)
+		.addSubcommandGroup(group =>
+			group
+				.setName('管理員')
+				.setDescription('設定管理員')
+				.addSubcommand(subcommand =>
+					subcommand
+						.setName('新增')
+						.setDescription('新增管理員')
+						.addUserOption(option =>
+							option
+								.setName('使用者')
+								.setDescription('新增的管理員')
+								.setRequired(true)
+						)
+				)
+				.addSubcommand(subcommand =>
+					subcommand
+						.setName('移除')
+						.setDescription('刪除管理員')
+						.addUserOption(option =>
+							option
+								.setName('使用者')
+								.setDescription('刪除的管理員')
+								.setRequired(true)
+						)
+				)
+		)
 
-	.addSubcommand(subcommand =>
-		subcommand
-			.setName('頻道')
-			.setDescription('Discord 的頻道設定')
-			.addChannelOption(option =>
-				option
-					.setName('控制台')
-					.setDescription('Discord 控制台的頻道')
-			)
-			.addChannelOption(option =>
-				option
-					.setName('指令紀錄')
-					.setDescription('Discord 指令紀錄的頻道')
-			)
-			.addChannelOption(option =>
-				option
-					.setName('下注紀錄')
-					.setDescription('Discord 下注紀錄的頻道')
-			)
-			.addChannelOption(option =>
-				option
-					.setName('狀態紀錄')
-					.setDescription('Discord 狀態紀錄的頻道')
-			)
-			.addChannelOption(option =>
-				option
-					.setName('綁定紀錄')
-					.setDescription('Discord 綁定紀錄的頻道')
-			)
-			.addChannelOption(option =>
-				option
-					.setName('錯誤紀錄')
-					.setDescription('Discord 錯誤紀錄的頻道')
-			)
-	),
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('頻道')
+				.setDescription('Discord 的頻道設定')
+				.addChannelOption(option =>
+					option
+						.setName('控制台')
+						.setDescription('Discord 控制台的頻道')
+				)
+				.addChannelOption(option =>
+					option
+						.setName('指令紀錄')
+						.setDescription('Discord 指令紀錄的頻道')
+				)
+				.addChannelOption(option =>
+					option
+						.setName('下注紀錄')
+						.setDescription('Discord 下注紀錄的頻道')
+				)
+				.addChannelOption(option =>
+					option
+						.setName('狀態紀錄')
+						.setDescription('Discord 狀態紀錄的頻道')
+				)
+				.addChannelOption(option =>
+					option
+						.setName('綁定紀錄')
+						.setDescription('Discord 綁定紀錄的頻道')
+				)
+				.addChannelOption(option =>
+					option
+						.setName('錯誤紀錄')
+						.setDescription('Discord 錯誤紀錄的頻道')
+				)
+		),
 
 	async execute(interaction) {
 		let config = JSON.parse(fs.readFileSync(`${process.cwd()}/config/config.json`, 'utf8'));
 		let user_data = await get_user_data(undefined, interaction.user.id);
-				
-		if (!config.whitelist || !user_data || user_data == 'Not Found' || user_data == 'Unexpected Error' ||!config.whitelist.includes(await get_player_name(user_data.player_uuid))) {
-            await interaction.reply({ content: '你沒有權限使用這個指令', ephemeral: true });
-            return;
-        }
-		
+
+		if (!config.whitelist || !user_data || user_data == 'Not Found' || user_data == 'Unexpected Error' || !config.whitelist.includes(await get_player_name(user_data.player_uuid))) {
+			await interaction.reply({ content: '你沒有權限使用這個指令', ephemeral: true });
+			return;
+		}
+
 		switch (interaction.options.getSubcommand()) {
 			case '對賭':
 				// check if the option is not blank
@@ -242,18 +242,18 @@ module.exports = {
 
 			case '建立':
 				config = JSON.parse(fs.readFileSync(`${process.cwd()}/config/config.json`, 'utf8'));
-				
+
 				if (!interaction.options.getString('文字或指令') || !interaction.options.getInteger('間隔時間')) {
 					await interaction.reply({ content: '請輸入文字', ephemeral: true })
 					return
-					
+
 				} else {
 					config.advertisement.push({
-						"text":interaction.options.getString('文字或指令'),
+						"text": interaction.options.getString('文字或指令'),
 						"interval": interaction.options.getInteger('間隔時間')
 					})
 				}
-				
+
 				fs.writeFileSync(`${process.cwd()}/config/config.json`, JSON.stringify(config, null, 4));
 				await interaction.reply({ content: '設定完成', ephemeral: true });
 				break;
@@ -271,22 +271,22 @@ module.exports = {
 						config.advertisement.splice(index, 1);
 					}
 				}
-				
+
 				fs.writeFileSync(`${process.cwd()}/config/config.json`, JSON.stringify(config, null, 4));
 				await interaction.reply({ content: '設定完成', ephemeral: true });
 				break;
-			
+
 			case '新增':
 				config = JSON.parse(fs.readFileSync(`${process.cwd()}/config/config.json`, 'utf8'));
 
-				if (!interaction.options.getUser('使用者')) {
+				if (!interaction.options.getString('使用者')) {
 					await interaction.reply({ content: '請輸入使用者', ephemeral: true })
 					return
 
 				} else {
-					config.whitelist.push(interaction.options.getUser('使用者').id);
+					config.whitelist.push(interaction.options.getString('使用者'));
 				}
-				
+
 				fs.writeFileSync(`${process.cwd()}/config/config.json`, JSON.stringify(config, null, 4));
 				await interaction.reply({ content: '設定完成', ephemeral: true });
 				break;
@@ -294,17 +294,16 @@ module.exports = {
 			case '移除':
 				config = JSON.parse(fs.readFileSync(`${process.cwd()}/config/config.json`, 'utf8'));
 
-				if (!interaction.options.getUser('使用者')) {
+				if (!interaction.options.getString('使用者')) {
 					await interaction.reply({ content: '請輸入使用者', ephemeral: true })
 					return
-
 				} else {
-					let index = config.whitelist.findIndex(x => x === interaction.options.getUser('使用者').id);
+					let index = config.whitelist.findIndex(x => x.toLowerCase() === interaction.options.getString('使用者').toLowerCase());
 					if (index !== -1) {
 						config.whitelist = config.whitelist.splice(index, 1);
 					}
 				}
-				
+
 				fs.writeFileSync(`${process.cwd()}/config/config.json`, JSON.stringify(config, null, 4));
 				await interaction.reply({ content: '設定完成', ephemeral: true });
 				break;
