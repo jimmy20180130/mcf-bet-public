912f5f60bcebc4822a6d10cb1ca99e365463ddab
Jimmy <68285469+jimmy20180130@users.noreply.github.com>
2024-11-14 23:10:36 +0800

    style: 加上一個 tab

--- error/mc_handler.js
+++ error/mc_handler.js
@@ -5,7 +5,7 @@ const { process_msg } = require(`../utils/process_msg.js`)
 async function mc_error_handler(bot, type, error_code, playerid, err_msg = '', uuid = '') {
     const messages = JSON.parse(fs.readFileSync(`${process.cwd()}/config/messages.json`, 'utf8'));
     const error_msg = messages.errors
-
+    
     await chat(bot, `/m ${playerid} ${await process_msg(bot, error_msg[type][error_code].replaceAll('%error_msg%', err_msg).replaceAll('%uuid%', uuid), playerid)}`)
 }
 
