e3da2c5cbfea4fc49d2851e2f1dbe647066e8b80
Jimmy <68285469+jimmy20180130@users.noreply.github.com>
2024-12-03 22:58:37 +0800

    fix: 修正在極短時間內簽到會重複給獎勵之問題

--- commands/daily.js
+++ commands/daily.js
@@ -28,15 +28,25 @@ module.exports = {
     }
 }
 
+let processList = []
+
 async function executeCommand(bot, playerid, args, client) {
     const messages = JSON.parse(fs.readFileSync(`${process.cwd()}/config/messages.json`, 'utf8'));
     const roles = JSON.parse(fs.readFileSync(`${process.cwd()}/config/roles.json`, 'utf8'));
     const config = JSON.parse(fs.readFileSync(`${process.cwd()}/config/config.json`, 'utf8'));
     const player_data = await get_user_data(await get_player_uuid(playerid))
 
+    if (processList.includes(playerid)) {
+        await chat(bot, `/m ${playerid} &c&l${await process_msg(bot, messages.commands.daily.already_signed, playerid)}`)
+        return;
+    }
+
+    processList.push(playerid)
+
     if (await canUseCommand(await get_player_uuid(playerid), args.split(' ')[0].toLowerCase())) {
         if (!config.discord.enabled) {
             await chat(bot, `/m ${playerid} &c&l${await process_msg(bot, messages.commands.daily.disabled, playerid)}`)
+            processList.splice(processList.indexOf(playerid), 1)
             return;
         }
 
@@ -53,12 +63,15 @@ async function executeCommand(bot, playerid, args, client) {
                 case 'Unexpected Error':
                     Logger.error(`[每日簽到] 玩家 ${playerid} 簽到時發生錯誤: ${daily_data}`)
                     await chat(bot, `/m ${playerid} &c&l${await process_msg(bot, messages.commands.daily.failed.replaceAll('%err%', 'Unexpected Error'), playerid)}`)
+                    processList.splice(processList.indexOf(playerid), 1)
                     return;
                 case 'Already Signed':
                     Logger.debug(`[每日簽到] 玩家 ${playerid} 已經簽到過了`)
                     await chat(bot, `/m ${playerid} &c&l${await process_msg(bot, messages.commands.daily.already_signed, playerid)}`)
+                    processList.splice(processList.indexOf(playerid), 1)
                     return;
                 default:
+                    processList.splice(processList.indexOf(playerid), 1)
                     break;
             }
 
@@ -76,6 +89,7 @@ async function executeCommand(bot, playerid, args, client) {
             if (!player_roles || player_roles.length == 0) {
                 Logger.debug(`[每日簽到] 玩家 ${playerid} 無簽到身份組`)
                 await chat(bot, `/m ${playerid} &c&l您目前無簽到金額可領取，如有疑問請詢問場地管理員`)
+                processList.splice(processList.indexOf(playerid), 1)
                 return
             }
 
@@ -98,4 +112,6 @@ async function executeCommand(bot, playerid, args, client) {
     } else {
         await mc_error_handler(bot, 'general', 'not_linked', playerid)
     }
+
+    processList.splice(processList.indexOf(playerid), 1)
 }
--- utils/db_write.js
+++ utils/db_write.js
@@ -22,24 +22,12 @@ function closeDB() {
 
 function executeQuery(query, params, callback) {
     let db = data
-    params = preventSQLInjection(params)
 
     db.serialize(() => {
         db.all(query, params, (err, rows) => {
             Logger.debug(`[資料庫] 已執行查詢："${query}" ，參數為 "${params}"`);
             callback(err, rows);
         });
-    });
-}
-
-function preventSQLInjection(param_array) {
-    return param_array.map(param => {
-      return param
-        .replace(/'/g, "''")            // 單引號轉成兩個單引號
-        .replace(/--/g, "")             // 移除雙連字符 (SQL註解)
-        .replace(/;/g, "")              // 移除分號
-        .replace(/[\x00-\x1f\x7f]/g, "") // 移除控制字符
-        .replace(/\\/g, "\\\\");        // 將反斜杠轉成兩個反斜杠
     });
 }
 
